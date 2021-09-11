d57a95664fe46699144ede58dffdff63e9ab0f1c
marceloapd <marcelo.apdassis@gmail.com>
2021-09-11 14:02:57 -0300

    Bling recebendo dados

--- services/axios.js
+++ services/axios.js
@@ -9,6 +9,17 @@ module.exports = {
         params: {
             api_token: config.PIPEDRIVE.api_token
         }
+    }),
+
+    bling: new axios.create({
+        baseURL: config.BLING.base_url,
+        params: {
+            apikey: config.BLING.api_token
+        },
+        headers: { 
+            'content-type': 'application/x-www-form-urlencoded' 
+        }
+
     })
 
 }
--- services/businessServices.js
+++ services/businessServices.js
@@ -2,7 +2,48 @@ const axios = require('./axios')
 
 module.exports = {
     async seedBling(){
-        const result = await axios.pipeDrive.get('deals?status=won')
-        return result.data
-    }       
+        const pipedriveDeals = await axios.pipeDrive.get('deals?status=won')
+        const deals = pipedriveDeals.data
+        var result = []
+
+        for(let element of deals.data){
+            let xml = `       
+            <?xml version="1.0" encoding="UTF-8"?>
+            <pedido>
+            <cliente>
+            <nome>${element.person_id.name}</nome>
+            <fone>${element.person_id.phone.value}</fone>
+            </cliente>
+            <itens>
+            <item>
+            <codigo>000</codigo>
+            <descricao>Caneta 001</descricao>
+            <un>Pç</un>
+            <qtde>10</qtde>
+            <vlr_unit>1.68</vlr_unit>
+            </item>
+            <item>
+            <codigo>000</codigo>
+            <descricao>Caderno 002</descricao>
+            <un>Un</un>
+            <qtde>3</qtde>
+            <vlr_unit>3.75</vlr_unit>
+            </item>
+            <item>
+            <codigo>000</codigo>
+            <descricao>Teclado 003</descricao>
+            <un>Cx</un>
+            <qtde>7</qtde>
+            <vlr_unit>18.65</vlr_unit>
+            </item>
+            </itens>
+            </pedido>`
+
+            let blingPedidos = await axios.bling.post(`pedido/json/?xml=${encodeURI(xml)}`)
+            result.push(blingPedidos.data)
+        }
+        
+        return result
+    } 
 }
+    
